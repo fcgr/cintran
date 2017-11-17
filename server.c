b2e6042cc5f9019707a125687c95943f9c21ece3
Felipe Guerra <fcgr@cin.ufpe.br>
2017-11-16 21:00:49 -0300

    teste de velocidade de busca na LAN

--- server.c
+++ server.c
@@ -3,7 +3,7 @@
 #include <string.h>
 #include <time.h>
 #include <errno.h>
-// sckts in linux libraries
+// sockets in linux libraries
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/socket.h>
@@ -13,14 +13,14 @@
 
 #define MAX_CLIENTS 20
 #define NDEPENDENCIES 3
-#define SERVER_PORT 54321
-#define CLIENT_PORT 65432
+#define SERVER_PORT 65432
+#define CLIENT_PORT 54321
 
 typedef union {
     struct { int id, condition; };
     struct { short left, forward, right; };
     //struct { long a, b, c, d, e, f, g, h; };
-} Message;
+} NetMssg;
 
 typedef struct a Client;
 typedef struct b Sign;
@@ -45,19 +45,19 @@ Sign signs[MAX_CLIENTS];
 void jkl (int i);
 
 int main (int argc, char **argv) {
-    struct sockaddr_in server_addr, client_addr;
+    struct sockaddr_in server_address, client_address;
     struct timespec now, then;
-    int sckt, msglen, i = 0, j, t, n_clients = 3, length;
+    int sockt, msglen, i = 0, j, t, n_clients = 3, length;
     long ini_time, dt;
-    Message mssg;
+    NetMssg mssg;
 
-    // creates a sckt
-    if ((sckt = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) < 0) jkl(1);
-    fcntl(sckt, F_SETFL, O_NONBLOCK); // non-blocking sckt
-    server_addr.sin_family = AF_INET; // address family;
-    server_addr.sin_addr.s_addr = htonl(INADDR_ANY); // any incoming interface
-    server_addr.sin_port = htons(SERVER_PORT); // local port number
-    if (bind(sckt, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) jkl(2);
+    // creates a sockt
+    if ((sockt = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) < 0) jkl(1);
+    fcntl(sockt, F_SETFL, O_NONBLOCK); // non-blocking sockt
+    server_address.sin_family = AF_INET; // address family;
+    server_address.sin_addr.s_addr = htonl(INADDR_ANY); // any incoming interface
+    server_address.sin_port = htons(SERVER_PORT); // local port number
+    if (bind(sockt, (struct sockaddr *)&server_address, sizeof(server_address)) < 0) jkl(2);
 
     // timing init
     printf("servidor ativo\n");
@@ -65,32 +65,56 @@ int main (int argc, char **argv) {
     ini_time = now.tv_sec;
     
     printf("size of Sign = %lu, size of Client = %lu\n", sizeof(Sign), sizeof(Client));
+    printf("grad's lan ip range = %d\n", 0xac1407ff - 0xac140401);
+
     // server loop
     while (1) {
         // timing
         then = now;
         if (clock_gettime(CLOCK_MONOTONIC, &now) != 0) jkl(4);
         dt = 1000000*(now.tv_sec - then.tv_sec) + (now.tv_nsec - then.tv_nsec)/1000; // in microseconds
-        //printf("t(%d) = %ld s; dt = %ld us\n", i++, (now.tv_sec - ini_time), dt);
+        printf("t(%d) = %ld s; dt = %ld us\n", i++, (now.tv_sec - ini_time), dt);
 
         // if msglen > sizeof(mssg), no message received is reported
-        while ((length = recvfrom(sckt, &mssg, sizeof(mssg), 0, (struct sockaddr *)&client_addr, &msglen)) > 0) {
-            if (length != sizeof(Message)) {
-                printf("message received from IP 0x%08x, port %d of bad length\n", client_addr.sin_addr.s_addr, client_addr.sin_port);
+        while ((length = recvfrom(sockt, &mssg, sizeof(mssg), 0, (struct sockaddr *)&client_address, &msglen)) > 0) {
+            if (length != sizeof(NetMssg)) {
+                printf("message received from IP 0x%08x, port %d of bad length\n", ntohl(client_address.sin_addr.s_addr), ntohs(client_address.sin_port));
                 //break;
             }
             for (i = 0; i < n_clients; i++) {
                 if (clients[i].id == mssg.id) break;
             }
             if (i == n_clients) {
-                printf("message received from UNKNOWN sign %d: condition = %d\n\tIP = 0x%08x, port %d", mssg.id, mssg.condition, client_addr.sin_addr.s_addr, client_addr.sin_port);
+                printf("message received from UNKNOWN sign %d: condition = %d\n\tIP = 0x%08x, port %d\n", mssg.id, mssg.condition, ntohl(client_address.sin_addr.s_addr), ntohs(client_address.sin_port));
             } else {
-                printf("message received from sign %d: condition = %d\n\tIP = 0x%08x, port %d", mssg.id, mssg.condition, client_addr.sin_addr.s_addr, client_addr.sin_port);
+                printf("message received from sign %d: condition = %d\n\tIP = 0x%08x, port %d\n", mssg.id, mssg.condition, ntohl(client_address.sin_addr.s_addr), ntohs(client_address.sin_port));
             }
         }
 
-        usleep(1000000/30); // 60 Hz
+        usleep(1000000/30); // 30 Hz
     }
+/*
+  // message sending iteration timing test
+
+    printf("0x%02x.%02x.%02x.%02x\n", 172, 20, 7, 255);
+    client_address.sin_family = AF_INET;
+    client_address.sin_port = htons(CLIENT_PORT);
+    clock_gettime(CLOCK_MONOTONIC, &then);
+    for (j = 0; j < 4; j++) {
+        for (i = 0xc0a80f01; i < 0xc0a80fff; i++) {
+            client_address.sin_addr.s_addr = htonl(i); 
+            while (sendto(sockt, &mssg, sizeof(mssg), 0, (struct sockaddr *)&client_address, sizeof(client_address)) != sizeof(mssg)) {
+                if (errno == 11) {
+                    printf("errno 11\n");
+                    usleep(500000);
+                } else jkl(5);
+            }
+        }
+    }
+    clock_gettime(CLOCK_MONOTONIC, &now);
+    dt = 1000000*(now.tv_sec - then.tv_sec) + (now.tv_nsec - then.tv_nsec)/1000;
+    printf("dt = %ld us\n", dt); // in microseconds
+*/
 
     return 0;
 }
@@ -99,3 +123,7 @@ void jkl (int i) {
     fprintf(stderr, "Error %d: %s (errno %d)\n", i, strerror(errno), errno);
     exit(i);
 }
+
+
+// for (i = 0xc0a80f01; i < 0xc0a80fff; i++) :: labhw && home
+// for (i = 0xac140401; i < 0xac1407ff; i++) :: grad
