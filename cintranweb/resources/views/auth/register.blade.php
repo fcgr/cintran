f9e554c83f8aa6989bb1c203cac3712fe057bace
Joao Paulo Vieira Cavalcante Monteiro <jpvcm@cin.ufpe.br>
2017-12-10 13:31:46 -0300

    Correcao Login

--- cintranweb/resources/views/auth/register.blade.php
+++ cintranweb/resources/views/auth/register.blade.php
@@ -26,7 +26,7 @@
                         </div>
 
                         <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label for="email" class="col-md-4 control-label">E-Mail</label>
+                            <label for="email" class="col-md-4 control-label">Login</label>
 
                             <div class="col-md-6">
                                 <input id="email" type="text" class="form-control" name="email" value="{{ $usuario->email or old('email') }}" required>
